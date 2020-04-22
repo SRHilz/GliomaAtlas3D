477c7b38d59f1d89fce87e72190bc0369b9a61f9
srhilz <stephanie.hilz@gmail.com>
2020-04-22 09:08:07 -0700

    updated plot3Dmodel example

--- R/plot3Dmodel.R
+++ R/plot3Dmodel.R
@@ -8,7 +8,7 @@
 #' @param sf sf number with sf lower
 #' @param colors vector of colors, usually corresponding to intensity of metric of interest; should be lenght of number of samples
 #' @param tumorModelsPath path to models
-#' @examples \donttest{\dontrun{plot3Dmodel('Patient300','sf10711',rep('#FF0000',10))}}
+#' @examples \donttest{\dontrun{plot3Dmodel('Patient300','sf10711',rep('#FF0000',10), tumorModelsPath)}}
 #' @return dolphins
 #' @export plot3Dmodel
 
